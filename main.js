ad3477e1996225444412283627c53c11afdf1ebb
martygogopooh-cmd <marty.go.go.pooh@gmail.com>
2026-04-30 18:40:52 +0000

    構築成功

---
+++ main.js
@@ -0,0 +1,18 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  })
+
+  // Googleなどを表示させてテスト
+  //win.loadURL('https://www.google.com')
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
