b1b8c6a4e0a36dc80231ec1d187bdc47d4baf441
mattynophoo-notepc <marty.go.go.pooh@gmail.com>
2026-05-07 05:24:33 +0000

    strictmode追加

--- src/renderer/src/index.tsx
+++ src/renderer/src/index.tsx
@@ -3,4 +3,8 @@ import ReactDOM from "react-dom/client";
 import "@/renderer/assets/style.css"; //先にcssを読み込む
 import { App } from "@/renderer/src/App"; // メインのUIコンポーネント
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
