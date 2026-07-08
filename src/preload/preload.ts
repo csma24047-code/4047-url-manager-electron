d096dfd24490ef3f13144ff4a04a8f43100b3b94
csma24047-code <csma24047@g.nihon-u.ac.jp>
2026-07-08 02:14:29 +0000

    前のdrag操作消す　regionを使えるようになったので

--- src/preload/preload.ts
+++ src/preload/preload.ts
@@ -3,12 +3,7 @@ import { contextBridge, ipcRenderer } from "electron";
 contextBridge.exposeInMainWorld("electronAPI", {
   // ウィンドウ操作用を追加
   send: (channel: string, data: any) => {
-    const validChannels = [
-      "window-control",
-      "window-drag-start",
-      "window-drag-move",
-      "window-drag-end",
-    ];
+    const validChannels = ["window-control"];
     if (validChannels.includes(channel)) {
       ipcRenderer.send(channel, data);
     }
