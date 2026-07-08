5f360b0d59e2af5b93cd45a97d26ad715f2a5638
martygogopooh-cmd <marty.go.go.pooh@gmail.com>
2026-07-08 07:40:34 +0000

    同文

--- src/renderer/src/index.tsx
+++ src/renderer/src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "@/renderer/assets/style.css"; //先にcssを読み込む
+import "@/renderer/style.css"; //先にcssを読み込む
 import { App } from "@/renderer/src/App"; // メインのUIコンポーネント
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
